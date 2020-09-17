04ed782a907c6abc32836bd29fa2210ab3ad1048
alkgrove <bob@alkgrove.com>
2020-09-17 14:21:08 -0700

    removed redundant folder

--- initmaker/initmaker/extended/inc/sdo_init.h
+++
@@ -1,5 +0,0 @@
-/* SDO */
-#define SWO_PRINT_PORT 0
-#define SWO_FREQUENCY 6000000UL
-#define SWOPRESCALE (((CPU_FREQUENCY)/(SWO_FREQUENCY)) - 1)
-#define ITM_ID ((1UL << ITM_TCR_TraceBusID_Pos) & ITM_TCR_TraceBusID_Msk)
